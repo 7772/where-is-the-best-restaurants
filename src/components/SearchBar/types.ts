5714904af06170bc99f1303c27104508a1e4d30c
landon <landon@olulo.io>
2020-07-09 13:20:00 +0900

    SearchBar 구현

---
+++ src/components/SearchBar/types.ts
@@ -0,0 +1,19 @@
+import {TextInputProps} from "react-native";
+
+interface SearchBarProps {
+  onPressSearch: (text: string) => void;  
+}
+
+interface SearchBarStates {
+  text: string;
+}
+
+interface ISearchBar extends TextInputProps {
+  onPress: () => void,
+}
+
+export {
+  SearchBarProps,
+  SearchBarStates,
+  ISearchBar,
+};
